9100fa36c5f34207039655c9e2b4ddae5e151e4f
GeniusR <2798135267@qq.com>
2025-07-09 15:06:33 +0800

    0709 1506zhanshu success

--- envs/JSBSim/tasks/__init__.py
+++ envs/JSBSim/tasks/__init__.py
@@ -4,3 +4,4 @@ from .singlecombat_with_missle_task import SingleCombatDodgeMissileTask, Hierarc
     HierarchicalSingleCombatShootTask, SingleCombatShootMissileTask
 from .tactical_template_test_task import TacticalTemplateTestTask
 from .TacticalTemplate import TacticalTemplate
+from .RefinedTacticalManeuvers import RefinedTacticalManeuvers
